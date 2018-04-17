4e873dba5118a2e87cb0f6773c6eff66a998d4fe
dit <dit@ditui-imac.local>
2018-04-17 19:18:23 +0900

    클로저 사용

--- UI Controller/ViewController.swift
+++ UI Controller/ViewController.swift
@@ -20,6 +20,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func cameraPressed(_ sender: Any) {
+        let myAlert = UIAlertController(title: "알림", message: "설정된 시간이 되었습니다", preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "확인", style: .default) { (myAction : UIAlertAction) in
+            self.view.backgroundColor = UIColor.yellow
+        }
+        
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel) { (myAction: UIAlertAction) in
+            self.view.backgroundColor = UIColor.white
+        }
+        
+        let deleteAction = UIAlertAction(title: "Delete", style: .destructive) { (myAction: UIAlertAction) in
+            self.view.backgroundColor = UIColor.green
+        }
+        
+        myAlert.addAction(okAction)
+        myAlert.addAction(cancelAction)
+        myAlert.addAction(deleteAction)
+        
+        present(myAlert, animated: true, completion: nil)
+        
+        
+    }
+    
 }
 
